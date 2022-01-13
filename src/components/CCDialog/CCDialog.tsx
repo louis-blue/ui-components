189886c4ceebd2ffed1509b2b6fc462b3b7c908c
SeungHyun Kim <k10130219@gmail.com>
2022-01-13 18:59:27 +0900

    LDialogContainer

--- src/components/CCDialog/CCDialog.tsx
+++ src/components/CCDialog/CCDialog.tsx
@@ -1,29 +1,54 @@
 import React, { RefObject, useEffect, useRef } from "react";
 import styled from "@emotion/styled";
 
-const LDialogContainer = styled.div`
-  position: fixed;
-  transition: opacity ease-in-out 0.2s;
-  z-index: 1300;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  background-color: rgba(0, 0, 0, 0.5);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-const LDialogContent = styled.div`
-  background-color: #fff;
-  border-radius: 4px;
-  width: ${(props: { width?: number; height?: number }) =>
-    props?.width || "none"};
-  width: ${(props: { width?: number; height?: number }) =>
-    props?.height || "none"};
-  box-shadow: 0 2px 1px -1px rgba(0, 0, 0, 0.2),
-    0 1px 1px 0px rgba(0, 0, 0, 0.14), 0 1px 3px 0px rgba(0, 0, 0, 0.12);
-`;
+// const LDialogContainer = styled.div`
+//   position: fixed;
+//   transition: opacity ease-in-out 0.2s;
+//   z-index: 1300;
+//   top: 0;
+//   bottom: 0;
+//   left: 0;
+//   right: 0;
+//   background-color: rgba(0, 0, 0, 0.5);
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+// `;
+
+const LDialogContainer = styled("div", {
+  label: "LDialogContainer",
+})(({ theme }) => {
+  return {
+    position: "fixed",
+    transition: "opacity ease-out 0.2s",
+    zIndex: 1300,
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  };
+});
+
+const LDialogContent = styled("div", {
+  label: "LDialogContent",
+  shouldForwardProp: (propName) => {
+    console.log(propName);
+    return true;
+  },
+})<{ width?: number; height?: number }>(({ theme, width, height }) => {
+  return {
+    backgroundColor: "white",
+    borderRadius: 4,
+    width: width || "none",
+    height: height || "none",
+    boxShadow:
+      "0 2px 1px -1px rgba(0, 0, 0, 0.20), 0 1px 1px 0px rgba(0, 0, 0, 0.14), 0 1px 3px 0px rgba(0, 0, 0, 0.12)",
+  };
+});
 
 interface Props {
   open: Boolean;
